9a605047374807d657d3fd2da3298a220d7eacba
vinog <vinogradskiy.n@gmail.com>
2025-09-23 12:04:08 +0200

    updates

--- config.py
+++ config.py
@@ -27,6 +27,9 @@ load_dotenv(os.path.join(ROOT_DIR, '.env'))
 ORCHESTRATOR_URL_PROD = "https://www.openfork.video"
 ORCHESTRATOR_URL_DEV = "http://localhost:3000"
 
+SUPABASE_URL = "https://vmuylzvwqravkmdmcpgv.supabase.co"
+SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtdXlsenZ3cXJhdmttZG1jcGd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxNDM3MjAsImV4cCI6MjA2NzcxOTcyMH0.f2USQOkuKhPksSLSXhTlyl5zTstyCyYvzdiHV9HQUKw"
+
 
 
 # Cache directory for assets and outputs
