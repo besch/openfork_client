b1d5864d033bebe7e1459247736b108f1f1911d9
vinog <vinogradskiy.n@gmail.com>
2025-10-22 21:51:42 +0200

    shutdownServerPort

--- utils/shutdown_handler.py
+++ utils/shutdown_handler.py
@@ -2,10 +2,10 @@ import threading
 import http.server
 import socketserver
 import logging
+import socket
 
 # Global event for graceful shutdown
 SHUTDOWN_EVENT = threading.Event()
-SHUTDOWN_SERVER_PORT = 8000 # TODO: Make configurable
 httpd_server = None # Global reference to the HTTP server
 
 class ShutdownHandler(http.server.BaseHTTPRequestHandler):
@@ -24,17 +24,28 @@ class ShutdownHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.end_headers()
 
+def find_free_port():
+    """Finds and returns an available TCP port."""
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('', 0))
+        return s.getsockname()[1]
+
 def start_shutdown_server():
     """Starts a simple HTTP server in a new thread to listen for shutdown requests."""
     handler = ShutdownHandler
     global httpd_server
-    with socketserver.TCPServer(('', SHUTDOWN_SERVER_PORT), handler, bind_and_activate=False) as httpd:
+    
+    port = find_free_port()
+    # We need to let the parent process know which port we are using
+    print(f"DGN_CLIENT_SHUTDOWN_SERVER_PORT:{port}", flush=True)
+
+    with socketserver.TCPServer(('', port), handler, bind_and_activate=False) as httpd:
         httpd.allow_reuse_address = True
         httpd_server = httpd
         try:
             httpd.server_bind()
             httpd.server_activate()
-            logging.info(f"Shutdown server started on port {SHUTDOWN_SERVER_PORT}")
+            logging.info(f"Shutdown server started on port {port}")
             httpd.serve_forever()
         except Exception as e:
             if not SHUTDOWN_EVENT.is_set():
